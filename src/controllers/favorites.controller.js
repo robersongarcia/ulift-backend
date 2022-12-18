b9c90cd6b118a4ef0fcfa23ca4b326c295d8523d
sexareds <cesarrojasv60@gmail.com>
2022-12-17 23:07:53 -0400

    changes made to getFavorites method

--- src/controllers/favorites.controller.js
+++ src/controllers/favorites.controller.js
@@ -4,12 +4,18 @@ const Favorite = require('../models/Favorites.js')(sequelize,DataTypes);
 
 const getFavorites = async (req, res, next) => {
   try {
-    const favorites = await Favorite.findAll({ where: { userID1: req.user.id } });
-      res.json({
-        success: true,
-        message: 'user favorites',
-        favorites: favorites
-      })
+    const [results, metadata] = await sequelize.query(
+      `SELECT u.nameU, u.photo, u.rate, COUNT(l.liftID) AS n_trips
+      FROM User u, Favorites f, Lift l
+      WHERE (f.userID2 = u.id)
+      AND (u.id = l.passengerID)
+      AND (userID1 = ${req.user.id})`
+    );
+    res.json({
+      success: true,
+      message: 'user favorites',
+      favorites: results
+    });
   } catch (error) {
     next(error);
   }
