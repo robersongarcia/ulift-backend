df151d523396df8e102c519cb2d126102da0aac7
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-01-12 18:19:32 -0400

    realizando procedimiento get con historial de colas

--- src/routes/lift.routes.js
+++ src/routes/lift.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getMatch, createLift } = require('../controllers/lift.controller');
+const { getMatch, createLift, liftHistory } = require('../controllers/lift.controller');
 
 
 router.get('/match/:lat/:lng', getMatch);
 
 router.post('/', createLift)
+
+router.get('/history', liftHistory);
 
 module.exports = router;
