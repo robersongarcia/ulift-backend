28d528add7d9f4f28752289495c15aae0c73ba19
Karen Morán <kamoran.19@est.ucab.edu.ve>
2023-01-13 08:13:36 -0400

    update route message

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use('/api', auth);
 app.use('/api/user', passport.authenticate('jwt', {session: false}), user);
 app.use('/api/favorites', passport.authenticate('jwt', {session: false}), favorites); 
 app.use('/api/lift', passport.authenticate('jwt', {session: false}), lift);
-app.use('/api/chat', passport.authenticate('jwt', {session: false}), messages);
+app.use('/api/messages', passport.authenticate('jwt', {session: false}), messages);
 
 app.listen(PORT, async () => {
     console.log(`App listening on port ${PORT}`);
